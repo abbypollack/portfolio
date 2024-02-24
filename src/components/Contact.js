0c7bc010af762fa6e3e3639d43cee70e96d076e6
Abbygail Pollack <abbypollack00@gmail.com>
2024-02-24 16:40:35 -0500

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact-page">
+      <h1>Contact Me</h1>
+      <p>You can reach me via email at example@example.com</p>
+      {/* Add more contact details or a form here */}
+    </div>
+  );
+}
+
+export default Contact;
